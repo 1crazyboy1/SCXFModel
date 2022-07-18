e28ed61bd57db843630c326a39f186936a613597
110 <471746662@qq.com>
2022-07-18 18:09:31 +0800

    3

--- SCXFModel/Form2.Designer.cs
+++ SCXFModel/Form2.Designer.cs
@@ -37,10 +37,10 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -89,6 +89,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "开始生产";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -98,6 +99,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "开始生产";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -107,6 +109,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "开始生产";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -116,6 +119,7 @@
             this.button4.TabIndex = 7;
             this.button4.Text = "开始生产";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // groupBox1
             // 
@@ -127,6 +131,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "消费者1";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 124);
+            this.textBox1.TabIndex = 0;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.textBox2);
@@ -137,6 +149,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "消费者2";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 20);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(446, 184);
+            this.textBox2.TabIndex = 1;
+            // 
             // button5
             // 
             this.button5.Location = new System.Drawing.Point(204, 524);
@@ -145,22 +165,7 @@
             this.button5.TabIndex = 10;
             this.button5.Text = "开始消费";
             this.button5.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(6, 20);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(446, 124);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(6, 20);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(446, 184);
-            this.textBox2.TabIndex = 1;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form2
             // 
--- SCXFModel/Form2.cs
+++ SCXFModel/Form2.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,12 +13,189 @@ namespace SCXFModel
 {
     public partial class Form2 : Form
     {
+        bool NeedStop = false;
+        bool NeedStop1 = false;
+        bool NeedStop2 = false;
+        bool NeedStop3 = false;
+        bool NeedStop4 = false;
+        List<int> RandomList = new List<int>();
+
+        
+
         public Form2()
         {
             InitializeComponent();
         }
 
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = (button2.Text == "开始生产") ? "停止生产" : "开始生产";
+            if (button1.Text=="停止生产")
+            {
+                Thread thread = new Thread(Thread1_Random);
+                thread.Start();
+                NeedStop1 = false;
+            }
+            else
+            {
+                NeedStop1 = true;
+            }
+                    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = (button2.Text == "开始生产") ? "停止生产" : "开始生产";
+            if (button2.Text == "停止生产")
+            {
+                Thread thread1 = new Thread(Thread2_Random);
+                thread1.Start();
+                NeedStop2 = false;
+            }
+            else
+            {
+                NeedStop2 = true;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = (button3.Text == "开始生产") ? "停止生产" : "开始生产";
+            if (button3.Text == "停止生产")
+            {
+                Thread thread3 = new Thread(Thread3_Random);
+                thread3.Start();
+                NeedStop3 = false;
+            }
+            else
+            {
+                NeedStop3 = true;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Text = (button4.Text == "开始生产") ? "停止生产" : "开始生产";
+            if (button4.Text == "停止生产")
+            {
+                Thread thread4 = new Thread(Thread4_Random);
+                thread4.Start();
+                NeedStop4 = false;
+            }
+            else
+            {
+                NeedStop4 = true;
+            }
+
+        }
+
+
+
+        private void Thread1_Random()
+        {
+            MYRandom ran = new MYRandom();
+            ran.IsEven += new IsEvenEvent(SaveDate1);
+            while (true)
+            {
+                if (NeedStop1==false)
+                {
+                    break;
+                }
+                ran.run_Random();
+                Thread.Sleep(200);
+            }
+                
+        }
+
+        private void Thread2_Random()
+        {
+            MYRandom ran = new MYRandom();
+            ran.IsEven += new IsEvenEvent(SaveDate1);
+            while (true)
+            {
+                if (NeedStop2 == false)
+                {
+                    break;
+                }
+                ran.run_Random();
+                Thread.Sleep(200);
+            }
+
+        }
+        private void Thread3_Random()
+        {
+            MYRandom ran = new MYRandom();
+            ran.IsEven += new IsEvenEvent(SaveDate1);
+            while (true)
+            {
+                if (NeedStop3 == false)
+                {
+                    break;
+                }
+                ran.run_Random();
+                Thread.Sleep(200);
+            }
+
+        }
+
+        private void Thread4_Random()
+        {
+            MYRandom ran = new MYRandom();
+            ran.IsEven += new IsEvenEvent(SaveDate1);
+            while (true)
+            {
+                if (NeedStop4 == false)
+                {
+                    break;
+                }
+                ran.run_Random();
+                Thread.Sleep(200);
+            }
+
+        }
+
+
+        private void SaveDate1(object sender,IsEvenEventArgs e)
+        {
+            lock (this)
+            {
+                RandomList.Add(e.Random);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Text = (button5.Text=="开始消费") ? "停止消费" : "开始消费";
+            if (button5.Text=="开始消费")
+            {
+                Thread MYTHREAD1 = new Thread(Customer1);
+
+                MYTHREAD1.Start();
+                Thread MYTHREAD2 = new Thread(Customer2);
+                MYTHREAD2.Start();
+                NeedStop = false;
+
+            }
+            else
+            {
+                NeedStop = true;
+            }
+        }
+
+
+        private void Customer1()
+        {
+
+        }
+
+        private void Customer2()
         {
 
         }
